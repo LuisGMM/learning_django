4ff54d0d7e332ae33edd3d60972acb2965d1244c
LuisGMM <emaildemiscuentas@gmail.com>
2023-01-08 12:27:13 +0100

    Done lots of stuff

--- src/learning_django/learning_django/settings.py
+++ src/learning_django/learning_django/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'store',
     'tags',
+    'likes',
 ]
 
 MIDDLEWARE = [
@@ -76,10 +77,20 @@ WSGI_APPLICATION = 'learning_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'learning_django',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'root'
     }
 }
 
